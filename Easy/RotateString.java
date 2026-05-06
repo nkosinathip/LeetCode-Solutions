d57f00db4ba48379ae0fabff84419e6035a73e11
Emmanuel Phiri <emsa9593@gmail.com>
2026-05-06 23:29:50 +0200

    Create RotateString.java
    A easy level code of rotating a string to find it matches with a given string

---
+++ Easy/RotateString.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        boolean status =false;
+        for(int i=0;i<s.length(); i++){
+            if(s.equals(goal)){
+                return true;
+            }
+
+            char firstletter= s.charAt(0);       
+            s=s.substring(1)+Character.toString(firstletter);
+          
+            if(s.equals(goal)){
+                status =true;
+            }
+            
+        }
+        return status;
+    }
+}
